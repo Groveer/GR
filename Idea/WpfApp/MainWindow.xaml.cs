103d2821eaa07f06dc792e9ecce56fd5cdd16f69
admin <groveer@outlook.com>
2020-01-08 12:31:16 +0800

    save

---
+++ Idea/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      List<int> lst = new List<int>()
+      {
+        1,2,4,3,5,8
+      };
+      //Lbx.ItemsSource = lst;
+      foreach(var n in lst)
+      {
+        Lbx.Items.Add(n);
+      }
+      Lbx.SelectedItem = 2;
+    }
+  }
+}
---
+++ Projects/HAMS/Models.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace HAMS
+{
+  public static class Global
+  {
+    public static string DbSource = $"Data Source={System.IO.Directory.GetCurrentDirectory()}/Data.db";
+  }
+  public class Login
+  {
+    public int Id { get; set; }
+    public string Login_Id { get; set; }
+    public string Employee_Id { get; set; }
+    public string Login_Name { get; set; }
+    public string Login_Pwd { get; set; }
+    public string Login_Power { get; set; }
+  }
+  public class LoginContext:DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite(Global.DbSource);
+    }
+    public DbSet<Login> Logins { get; set; }
+  }
+}
